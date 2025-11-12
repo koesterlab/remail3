f899c4d6d933a4832a745995f76f47c8a284e638
Pola Eskandar <pola.eskandar@codingstreamer.com>
2025-11-12 02:01:43 +0100

    add more checks to the ci

--- remail/database/models.py
+++ remail/database/models.py
@@ -19,6 +19,7 @@ class Contact(SQLModel, table=True):
     id: int | None = id_field("contact")
     email_address: str
     name: str | None = None
+
     receptions: list["EmailReception"] = Relationship(back_populates="contact")
     sent_emails: list["Email"] = Relationship(back_populates="sender")
 
@@ -33,6 +34,7 @@ class EmailReception(SQLModel, table=True):
     email_id: int = Field(foreign_key="email.id", primary_key=True)
     contact_id: int = Field(foreign_key="contact.id", primary_key=True)
     kind: RecipientKind
+
     email: "Email" = Relationship(back_populates="recipients")
     contact: "Contact" = Relationship(back_populates="receptions")
 
@@ -41,6 +43,7 @@ class Attachment(SQLModel, table=True):
     id: int | None = id_field("attachment")
     filename: str
     email_id: int = Field(default=None, foreign_key="email.id")
+
     email: "Email" = Relationship(back_populates="attachments")
 
 
@@ -51,10 +54,11 @@ class Email(SQLModel, table=True):
     sender: "Contact" = Relationship(back_populates="sent_emails")
     subject: str
     body: str
-    attachments: list[Attachment] | None = Relationship(back_populates="email")
-    recipients: list[EmailReception] = Relationship(back_populates="email")
     date: datetime
     urgency: int | None
+
+    attachments: list[Attachment] | None = Relationship(back_populates="email")
+    recipients: list[EmailReception] = Relationship(back_populates="email")
 
 
 class Protocol(Enum):
